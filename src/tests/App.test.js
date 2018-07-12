4f435292b56bbfe9e2bd82b1211bb8d788a7e846
Patrick Sharkey <patrick.sharkey@thehumangeo.com>
2018-07-12 09:57:54 -0400

    updated with jest and enzyme, created simple test case for App component to test functionality

---
+++ src/tests/App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from '../components/App';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
